9832e662a693bc650810c56edba1329bba948c16
Kyle <kyleyesmariam@icloud.com>
2025-02-05 17:15:00 +1300

    updated config

--- init.lua
+++ init.lua
@@ -618,7 +618,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        --         python-lsp-server = {},
+        -- python-lsp-server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
